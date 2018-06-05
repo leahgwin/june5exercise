94cad2017c03f78fdb6c501adfdfa45c96bb27a6
leahgwin <leahgwin@gmail.com>
2018-06-05 09:11:31 -0500

    added comments

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 // 1. Write a function that takes two arguments. This function should return the concatenated value of the two arguments.
-// 2. Call the function with your first name and last name as the two arguments.
-// 3. Console log the return value.
-
 const naming = (first, last) => {
+    // 2. Call the function with your first name and last name as the two arguments.
     const name = first + " " + last;
     return name;
 }
+
+// 3. Console log the return value.
 
 console.log(naming("leah", "gwin"));
